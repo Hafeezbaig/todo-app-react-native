0ee03a9b518de42798a743f8b2907bcdea964154
Hafeezbaig <hafeezbaig3138@gmail.com>
2026-01-31 18:48:08 +0530

    first commit - base code

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.content}>Edit app/index.tsx to edit this screen.</Text>
+      {/* <Link href="/about">Go to About</Link> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    fontSize: 18,
+  }
+});
