e2069f65bfb74dea52186b6cb848c31d267a7b02
Hafeezbaig <hafeezbaig3138@gmail.com>
2026-02-02 07:30:04 +0530

    Dark and light toggle mode implemented

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,18 @@
 import { Tabs } from 'expo-router'
 import IonIcons from '@expo/vector-icons/Ionicons'
+import useTheme from '@/hooks/useTheme'
 
 const TabsLayout = () => {
+  const {colors} = useTheme();
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: "red",
-        tabBarInactiveTintColor: "gray",
-        tabBarStyle: { backgroundColor: "#222",
+        tabBarActiveTintColor: colors.primary,
+        tabBarInactiveTintColor: colors.textMuted,
+        tabBarStyle: { backgroundColor: colors.surface,
           borderTopWidth: 1,
-          borderTopColor: "yellow",
+          borderTopColor: colors.border,
           height: 90,
           paddingBottom: 30,
           paddingTop: 10,
